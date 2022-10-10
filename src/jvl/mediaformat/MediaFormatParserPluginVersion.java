b0c07ef8dde974b36b713d8686591809c65e80f7
jvl711 <jvl711@workstation.core.joshandmonique.com>
2022-10-09 20:33:23 -0400

    Fixed depend download for linux

--- src/jvl/mediaformat/MediaFormatParserPluginVersion.java
+++ src/jvl/mediaformat/MediaFormatParserPluginVersion.java
@@ -3,8 +3,8 @@ package jvl.mediaformat;
 
 public class MediaFormatParserPluginVersion
 {
-    private final static String BUILDTIME = "10/09/2022 20:18:46";
-    private final static String BUILDNUMBER = "110";
+    private final static String BUILDTIME = "10/09/2022 20:32:21";
+    private final static String BUILDNUMBER = "118";
     private final static String VERSION = "0.7";
     
     public static String getVersion()
