ec35769e1a8e9e86ca327a0a52ca23823a44b423
Joshua Lewis <jvl711@gmail.com>
2022-10-15 08:11:22 -0400

    Initial Pipeline build

--- src/jvl/mediaformat/MediaFormatParserPluginVersion.java
+++ src/jvl/mediaformat/MediaFormatParserPluginVersion.java
@@ -3,8 +3,8 @@ package jvl.mediaformat;
 
 public class MediaFormatParserPluginVersion
 {
-    private final static String BUILDTIME = "10/10/2022 19:33:24";
-    private final static String BUILDNUMBER = "126";
+    private final static String BUILDTIME = "10/15/2022 07:47:25";
+    private final static String BUILDNUMBER = "129";
     private final static String VERSION = "0.7";
     
     public static String getVersion()
